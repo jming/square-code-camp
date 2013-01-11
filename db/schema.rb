d7a0eaf7a28eaa3887d860d4a2877be6849a8862
Jennifer Qiao <jenniferq0204@gmail.com>
2013-01-11 12:42:18 -0800

    header with login & logout working properly

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130110022543) do
+ActiveRecord::Schema.define(:version => 20130111202022) do
 
   create_table "event_attendees", :force => true do |t|
     t.integer  "user_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20130110022543) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "name"
   end
 
 end
