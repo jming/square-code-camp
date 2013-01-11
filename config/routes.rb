881964e2cdf2e9999ca90eb2f01707116df94ec1
Square <github@squareup.com>
2013-01-11 10:36:05 -0800

    link events list to show and change route for events

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Platz::Application.routes.draw do
   root :to => "events#index"
 
   resources :sessions
+  resources :events
   resources :users do
-    resources :events
     post 'attend'
   end
 
